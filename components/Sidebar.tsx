24a486d598a4eff59ec75052d6badd2f62e29556
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 10:53:34 -0400

    feat: Loader component added - className to Image (next/image)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,23 +1,31 @@
 import Image from 'next/image'
 import Categoria from './Categoria'
+import Loader from './Loader'
 import useKiosko from '@/hooks/useKiosko'
 
 const Sidebar = () => {
-  const { categorias } = useKiosko()
+  const { categorias, loading } = useKiosko()
 
   return (
     <>
       <Image
-        width={300}
+        width={160}
         height={100}
         src="/assets/img/logo.svg"
         alt="Imagen logotipo"
+        className='mx-auto my-4'
       />
 
-      <nav className="mt-10">
-        {categorias.map(categoria => (
-          <Categoria key={categoria.id} categoria={categoria} />
-        ))}
+      <nav className="mt-5">
+        <ul>
+          {loading ? (
+            <Loader />
+          ) : (
+            categorias.map(categoria => (
+              <Categoria key={categoria.id} categoria={categoria} />
+            ))
+          )}
+        </ul>
       </nav>
     </>
   )
