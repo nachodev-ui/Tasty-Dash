b2d3e1922bd06e5d71048730995856717580005c
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:11:19 -0400

    feat: Custom hook useKiosko to access the context of Kiosko - New interface with categoriaActual (extends IKioskoContext)

--- hooks/useKiosko.tsx
+++ hooks/useKiosko.tsx
@@ -1,8 +1,20 @@
 import { useContext } from 'react'
+import { IKioskoContext } from '@/types/types'
 import KioskoContext from '@/context/KioskoProvider'
+import CategoriaConProductos from '@/customType'
 
-const useKiosko = () => {
-  return useContext(KioskoContext)
+/**
+ * Interfaz personalizada que extiende la interfaz 'IKioskoContext' y reemplaza
+ * la propiedad 'categoriaActual' con 'CategoriaConProductos'.
+ */
+interface IKioskoContextConProductos extends IKioskoContext {
+  categoriaActual: CategoriaConProductos | undefined
+}
+
+const useKiosko = (): IKioskoContextConProductos => {
+  const kioskoContext = useContext(KioskoContext) as IKioskoContextConProductos
+
+  return kioskoContext
 }
 
 export default useKiosko
