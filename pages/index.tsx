faf16e49c6e85cea9e9699b9ce3d18d1db20d518
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:23:36 -0400

    feat: The home page displays the products selected by category - Rendering Producto component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import Layout from '@/layout/Layout'
+import Producto from '@/components/Producto'
+import useKiosko from '@/hooks/useKiosko'
 
 export default function Home() {
+  const { categoriaActual } = useKiosko()
 
   return (
-    <Layout pagina='Inicio'>
-      <h1>Inicio</h1>
+    <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
+      <h1 className="text-5xl font-black">{categoriaActual?.nombre}</h1>
+
+      <p className="text-xl mt-8 mb-6">Personaliza tu pedido a continuación</p>
+
+      <div className="grid gap-4 grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {categoriaActual?.productos?.map(producto => (
+          <Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
     </Layout>
   )
 }
