75ab55af0d9e92595ff5ced34afc9cd33b67806f
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:14:22 -0400

    feat: Added component Steps, react-toastify and modals in the Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,31 @@
 import { ReactNode } from 'react'
+import Modal from 'react-modal'
+import { ToastContainer } from 'react-toastify'
 import Head from 'next/head'
+import Pasos from '@/components/Pasos'
 import Sidebar from '@/components/Sidebar'
+import ModalProducto from '@/components/ModalProducto'
+import useKiosko from '@/hooks/useKiosko'
+import 'react-toastify/ReactToastify.css'
+
+const pasos = [
+  { paso: 1, nombre: 'Menú', url: '/' },
+  { paso: 2, nombre: 'Resumen', url: '/resumen' },
+  { paso: 3, nombre: 'Datos y Total', url: '/total' },
+]
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}
+
+Modal.setAppElement('#__next')
 
 interface ILayout {
   children: ReactNode
@@ -8,10 +33,12 @@ interface ILayout {
 }
 
 const Layout = ({ children, pagina }: ILayout) => {
+  const { modal } = useKiosko()
+
   return (
     <>
       <Head>
-        <title>Café - {pagina}</title>
+        <title>Tasty Dash - {pagina}</title>
         <meta name="description" content="Kiosco, cafetería." />
       </Head>
 
@@ -21,9 +48,24 @@ const Layout = ({ children, pagina }: ILayout) => {
         </aside>
 
         <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          {children}
+          <div className="p-10">
+            <Pasos pasos={pasos} />
+            {children}
+          </div>
         </main>
       </div>
+
+      {
+        <Modal
+          isOpen={modal}
+          style={customStyles}
+          contentLabel="Modal producto"
+        >
+          <ModalProducto />
+        </Modal>
+      }
+
+      <ToastContainer autoClose={3000} />
     </>
   )
 }
