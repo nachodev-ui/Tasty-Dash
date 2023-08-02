b381d9b5de326a46765f9824fcc35ae6c47f7190
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:16:52 -0400

    feat: Updated API path to include products in the response

--- pages/api/categorias.ts
+++ pages/api/categorias.ts
@@ -1,13 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
-import { Categoria } from '@/types/interfaces'
+// pages/api/categorias.ts
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Categoria, PrismaClient } from '@prisma/client'
 
 const prisma: PrismaClient = new PrismaClient()
 
+// Utilizando el tipo de dato Categoria de Prisma Client (npx prisma generate)
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Categoria[]>) {
-
-  const categorias = await prisma.categoria.findMany()
-
-  res.status(200).json(categorias)
+  try {
+    const categorias: Categoria[] = await prisma.categoria.findMany({
+      include: {
+        productos: true
+      }
+    })
+    res.status(200).json(categorias)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json([])
+  }
 }
