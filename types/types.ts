d6b77121c0a0383db39086e5b217fe8adc4d17c1
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:38:46 -0400

    feat: Definitions of types and interfaces for the Kiosk context

---
+++ types/types.ts
@@ -0,0 +1,52 @@
+// Types for interfaces
+import { SyntheticEvent } from 'react'
+import { Categoria, Producto } from '@prisma/client'
+
+interface ProductoSinCategoria extends Producto {}
+
+// Definir una nueva interfaz ProductoConCantidad que extienda ProductoSinImagenCategoria
+export interface ProductoConCantidad extends ProductoSinCategoria {
+  cantidad: number;
+}
+
+export interface IKioskoContext {
+  loading: boolean
+  modal: boolean
+  nombre: string
+  total: number | 0
+  setNombre: SetNombreFn
+  pedido: ProductoConCantidad[]
+  producto: Producto | undefined
+  categorias: Categoria[]
+  categoriaActual: Categoria | undefined
+  obtenerCategoriaActual: ObtenerCategoriaActualFn
+  handleSetProducto: HandleSetProductoFn
+  handleChangeModal: HandleChangeModalFn
+  handleEditarCantidades: HandleEditarCantidadesFn
+  handleEliminarProducto: HandleEliminarProductoFn
+  handleAgregarPedido: HandleAgregarPedidoFn
+  handleEnviarPedido: HandleEnviarPedidoFn
+}
+
+export interface Paso {
+  paso: number
+  nombre: string
+  url: string
+}
+
+export type ObtenerCategoriaActualFn = (id: number) => void
+
+export type HandleSetProductoFn = (producto: Producto) => void
+
+export type HandleChangeModalFn = () => void
+
+export type SetNombreFn = (nombre: string) => void
+
+export type HandleEditarCantidadesFn = (id: number) => void
+
+export type HandleEliminarProductoFn = (id: number) => void
+
+export type HandleEnviarPedidoFn = (e: SyntheticEvent) => Promise<void>
+
+// eslint-disable-next-line no-unused-vars
+export type HandleAgregarPedidoFn = (producto: ProductoConCantidad, cantidad: number) => void
