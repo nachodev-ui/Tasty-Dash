5234ddaa2cb4508117d46e7bf35bf0d0ff9c2474
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 10:47:51 -0400

    feat: Svg close icon (X) component

---
+++ components/CloseIcon.tsx
@@ -0,0 +1,20 @@
+const CloseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-10 h-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  )
+}
+
+export default CloseIcon
