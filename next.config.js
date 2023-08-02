678f5005a4aa5a965bf53724f3ce2e463199def2
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-02 11:47:06 -0400

    feat: Default image configuration in next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost:3000'],
+    formats: ['image/avif', 'image/webp'],
+  }
 }
 
 module.exports = nextConfig
